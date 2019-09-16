73ca23fdd37b8017e8ba3c26dda098369f6c7a59
ads6495 <astrange55@icloud.com>
2019-09-16 14:19:17 -0400

    ready for data

---
+++ Safari.cs
@@ -0,0 +1,13 @@
+namespace SafariAdventure
+{
+  public class Safari
+  {
+    public int Id { get; set; }
+
+    public string Species { get; set; }
+
+    public int CountOfTimesSeen { get; set; }
+
+    public string LocationOfLastSeen { get; set; }
+  }
+}
